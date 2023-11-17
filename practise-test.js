1c70f635408e6ae0da767b367c70c53f3b1a0dae
jahid <jahidjob5@outlook.com>
2023-11-17 23:17:09 +0300

    SimpleInterest

--- practise-test.js
+++ practise-test.js
@@ -6,18 +6,18 @@
 ======================================= */
 
 
-function temperture(degrees) {
+/* function temperture(degrees) {
     sum = (degrees * 9 / 5) + 32;
     return sum;
 }
 let heat = 1;
 const OutputTemp = temperture(heat);
 console.log("Your Temperture converted are ", OutputTemp, "Degress Farenheight");
-
+ */
 
 /* 
 =======================================
-                            Convert farenheight to se Celsius.
+                            Convert farenheight to Celsius.
 ======================================= */
 
 /* 
@@ -29,3 +29,29 @@ function temperture(degrees) {
 let bodyTemp = 98.4;
 const OutputBody = temperture(98);
 console.log(OutputBody); */
+
+
+/*=======================================
+                        SIMPLE INTEREST  6%
+======================================= */
+
+
+function principal(money) {
+
+    let month6 = money * 6;
+    yearlyInt = month6 / 100;
+    threeYearsInt = yearlyInt * 3;
+    totalInt = money + threeYearsInt
+
+    return totalInt;
+    // console.log(month6, yearlyInt, threeYearsInt, totalInt);
+}
+let mymoney = 20000;
+const myInt = principal(mymoney);
+console.log("your moneys 6% Interest", myInt, "$");
+
+let yourmoney = 30000;
+const yourInt = principal(yourmoney);
+console.log("your moneys 6% Interest", yourInt, "$");
+
+
